c05aa5377b863527b6c823179fe5cb5f418806f5
ToeyJSD9 <toeykenji@gmail.com>
2025-03-10 11:10:55 +0700

    Add files via upload
    ตะกี้ส่งผิดครับ

---
+++ script.js
@@ -0,0 +1,11 @@
+function Sayhi(name) {
+    return `Hello, ${name}`;
+  }
+  
+ const Sayhi = function (name) {
+    return `Hello, ${name}`;
+ } 
+ const Sayhi = (name) => {
+    return `Hello, ${name}`;
+  };
+  console.log(greetPerson("Toey"));
